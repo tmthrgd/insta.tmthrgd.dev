dab7634dec18b8b3af1116eec5d2293f32858daa
Tom Thorogood <me+github@tomthorogood.co.uk>
2019-05-09 23:23:42 +0930

    Add GZIP compression

--- insta.go
+++ insta.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/tmthrgd/gziphandler"
 	handlers "github.com/tmthrgd/httphandlers"
 )
 
@@ -16,6 +17,7 @@ func main() {
 	router := chi.NewRouter()
 	router.Use(
 		middleware.GetHead,
+		gziphandler.Wrapper(),
 		handlers.SecurityHeadersWrap(&handlers.SecurityHeaders{
 			ContentSecurityPolicy: "default-src 'none'; script-src 'self'; style-src 'self' https:; img-src 'self' https:; font-src https:; frame-ancestors 'none'; block-all-mixed-content; sandbox allow-forms allow-scripts; base-uri 'none'; report-uri https://tomthorogood.report-uri.com/r/d/csp/enforce",
 		}),
