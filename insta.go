9cbc6926c44ff591b89e8141e8148b56fc9be6a8
Tom Thorogood <me+github@tomthorogood.co.uk>
2019-05-12 21:05:53 +0930

    Move gziphandler.Wrapper to end of router.Use
    This should be applied after the security headers.

--- insta.go
+++ insta.go
@@ -17,11 +17,11 @@ func main() {
 	router := chi.NewRouter()
 	router.Use(
 		middleware.GetHead,
-		gziphandler.Wrapper(),
 		handlers.SecurityHeadersWrap(&handlers.SecurityHeaders{
 			ContentSecurityPolicy: "default-src 'none'; script-src 'self'; style-src 'self' https:; img-src 'self' https:; font-src https:; frame-ancestors 'none'; block-all-mixed-content; sandbox allow-forms allow-scripts; base-uri 'none'; report-uri https://tomthorogood.report-uri.com/r/d/csp/enforce",
 		}),
 		handlers.SetHeaderWrap("Server", "insta.tmthrgd.dev"),
+		gziphandler.Wrapper(),
 	)
 	router.NotFound(notFoundHandler())
 
