88d2a17b3bbf68f857cb6c683a0a55f31af9a043
Tom Thorogood <me+github@tomthorogood.co.uk>
2019-06-01 12:23:04 +0930

    Add more security headers from go.tmthrgd.dev

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/shurcooL/httpfs v0.0.0-20181222201310-74dc9339e414
 	github.com/shurcooL/vfsgen v0.0.0-20181202132449-6a9ea43bcacd
 	github.com/tmthrgd/gziphandler v0.0.0-20190303121617-ae837a951453
-	github.com/tmthrgd/httphandlers v0.0.0-20190513021200-f1d2c578ad2b
+	github.com/tmthrgd/httphandlers v0.0.0-20190601024710-973f5c703913
 	github.com/tmthrgd/httputils v0.0.0-20190303111359-ec3de85dbccd // indirect
 	go.tmthrgd.dev/vfshash v0.0.0-20190531093011-cc3700bd94ce
 	golang.org/x/net v0.0.0-20190503192946-f4e77d36d62c
--- insta.go
+++ insta.go
@@ -20,6 +20,11 @@ func main() {
 		middleware.GetHead,
 		handlers.SecurityHeadersWrap(&handlers.SecurityHeaders{
 			ContentSecurityPolicy: "default-src 'none'; script-src 'self'; style-src 'self' https:; img-src 'self' https:; font-src https:; frame-ancestors 'none'; block-all-mixed-content; sandbox allow-forms allow-scripts; base-uri 'none'; report-uri https://tomthorogood.report-uri.com/r/d/csp/enforce",
+
+			StrictTransportSecurity: "max-age=31536000; includeSubDomains; preload",
+
+			ReportTo: `{"group":"default","max_age":31536000,"endpoints":[{"url":"https://tomthorogood.report-uri.com/a/d/g"}],"include_subdomains":true}`,
+			NEL:      `{"report_to":"default","max_age":31536000,"include_subdomains":true}`,
 		}),
 		handlers.SetHeaderWrap("Server", "insta.tmthrgd.dev"),
 		gziphandler.Wrapper(),
